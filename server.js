d6a39a6ff626ced104be21180a611b31348229af
Pat Jacobs <musicbender@gmail.com>
2017-02-19 18:33:08 -0800

    not able to sve to the database using post request fromt the form

--- server.js
+++ server.js
@@ -4,21 +4,28 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
+const dbUrl = "mongodb://admin:5Vg4&W$baf@ds157499.mlab.com:57499/firefly-quotes";
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect('https://mlab.com/databases/firefly-quotes', (err, database) => {
+MongoClient.connect(dbUrl, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
 
+  app.listen('3000', () => {
+    console.log('Listening to 3000');
+  });
 });
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
-
 });
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
-});
+  db.collection("quotes").save(req.body, (err, result) => {
+    if (err) return console.log(err);
 
-app.listen('3000', () => {
-  console.log('Listening to 3000');
+    console.log("Saved to database");
+    res.redirect('/');
+  });
 });
